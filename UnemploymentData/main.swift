4d95610256a605c358bbf2f4424804424a86a1d8
Paul Lemelle <Paul.Lemelle@yahoo.com>
2018-07-08 13:01:08 -0700

    Initial Commit

---
+++ UnemploymentData/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  UnemploymentData
+//
+//  Created by Paul Lemelle on 7/8/18.
+//  Copyright © 2018 Paul Lemelle. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
